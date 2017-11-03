da556c5309d729ee185db0aa3f2f8e3eddb7fa8b
Edward Bai <edwardbai31@gmail.com>
2017-11-02 19:29:40 -0700

    Complete phase 1

--- W5D4/JSIntro.js
+++ W5D4/JSIntro.js
@@ -67,3 +67,28 @@ function fizzBuzz (arr) {
 
   return fizzBuzzArr;
 }
+
+function isPrime (n) {
+  if (n < 2) { return false; }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sumOfNPrimes (n) {
+  let sum = 0;
+  let countPrimes = 0;
+  let i = 2;
+
+  while (countPrimes < n) {
+    if (isPrime(i)) {
+      sum += i;
+      countPrimes++;
+    }
+    i++;
+  }
+  return sum;
+}
