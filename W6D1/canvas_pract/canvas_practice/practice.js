61a48fad3224c8eca2896f68907e23dbf9c553c8
Edward Bai <edwardbai31@gmail.com>
2017-11-06 22:05:49 -0800

    Fix pathing on script tags

--- W6D1/canvas_pract/canvas_practice/practice.js
+++ W6D1/canvas_pract/canvas_practice/practice.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var canvas = document.getElementById('myCanvas');
+  var canvas = document.getElementById('mycanvas');
   canvas.width = 500;
   canvas.height = 500;
 
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = 'red';
-  ctx.fillRect = (0,0,500,500);
+  ctx.fillRect(0,0,500,500);
   ctx.beginPath();
   ctx.arc(75, 75, 50, 0, 2 * Math.PI);
 });
